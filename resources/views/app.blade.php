0c9f62684e491d6208d412c243b7762cbf88444c
algm <piticonejo@gmail.com>
2018-01-28 13:22:32 +0100

    remove cdns

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,8 +18,6 @@
     </div>
 
     <!-- Scripts -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.min.js"></script>
-  	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.9/es5-shim.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
